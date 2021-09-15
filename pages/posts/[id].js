e7179e7d621d1fd79912d33ef56aeb97ac53e6bc
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-14 20:54:41 -0700

    removed duplicated functions

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -26,14 +26,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
-}
-
-export async function getStaticPaths() {
-  // Return a list of possible value for id
-}
-
-export async function getStaticProps({ params }) {
-  // Fetch necessary data for the blog post using params.id
 }
